285cc4b3d16b31be887bf112423ecac43128e52f
cat-feng <feng.luo@qq.com>
2023-08-04 01:39:08 +0800

    add: validation

--- user-app/src/main/java/cloud/ffeng/user/app/command/service/UserLoginAppService.java
+++ user-app/src/main/java/cloud/ffeng/user/app/command/service/UserLoginAppService.java
@@ -6,6 +6,8 @@ import cloud.ffeng.user.facade.request.UserLoginByPlatformAuthRequest;
 import cloud.ffeng.user.facade.request.UserLoginByVerificationRequest;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
+
 /**
  * @author cat-feng
  */
@@ -18,7 +20,7 @@ public interface UserLoginAppService {
      * @param request 用户登录请求信息
      * @return 登录请求结果信息
      */
-    UserLoginFlowDTO loginByPassword(UserLoginByPasswordRequest request);
+    UserLoginFlowDTO loginByPassword(@Valid UserLoginByPasswordRequest request);
 
     /**
      * 用户登录---验证码登录
@@ -26,7 +28,7 @@ public interface UserLoginAppService {
      * @param request 用户验证码登录请求信息
      * @return 登录请求结果信息
      */
-    UserLoginFlowDTO loginByVerification(UserLoginByVerificationRequest request);
+    UserLoginFlowDTO loginByVerification(@Valid UserLoginByVerificationRequest request);
 
     /**
      * 用户登录---平台授权登录
@@ -34,6 +36,6 @@ public interface UserLoginAppService {
      * @param request 用户平台授权登录请求信息
      * @return 登录请求结果信息
      */
-    UserLoginFlowDTO loginByPlatformAuth(UserLoginByPlatformAuthRequest request);
+    UserLoginFlowDTO loginByPlatformAuth(@Valid UserLoginByPlatformAuthRequest request);
 
 }
--- user-app/src/main/java/cloud/ffeng/user/app/query/service/impl/UserQueryAppServiceImpl.java
+++ user-app/src/main/java/cloud/ffeng/user/app/query/service/impl/UserQueryAppServiceImpl.java
@@ -1,10 +1,16 @@
 package cloud.ffeng.user.app.query.service.impl;
 
 import cloud.ffeng.user.app.query.service.UserQueryAppService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 /**
  * @author cat-feng
  */
+@Slf4j
+@Service
+@AllArgsConstructor
 public class UserQueryAppServiceImpl implements UserQueryAppService {
 
 }
